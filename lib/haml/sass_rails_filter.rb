8aaea4bff7771725fa68706520be88d1dec2b688
Brant Wedel <brant@bitbased.net>
2013-03-14 14:16:57 -0500

    updated sass rails filter to use the proper asset context in order to fix a probelm with @import

--- lib/haml/sass_rails_filter.rb
+++ lib/haml/sass_rails_filter.rb
@@ -4,7 +4,7 @@ module Haml
     # Rails's asset helpers to be used inside Haml Sass filter.
     class SassRailsTemplate < ::Sass::Rails::SassTemplate
       def render(scope=Object.new, locals={}, &block)
-        scope = ::Rails.application.assets.context_class.new(::Rails.application.assets, "/", "/")
+        scope = ::Rails.application.assets.context_class.new(::Rails.application.assets, "/", Rails.application.assets.root)
         super
       end
 
